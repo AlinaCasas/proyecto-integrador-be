6fe128379c2432edc38bcef7372186c20416460f
Fabricio <fgonzalezguasque@gmail.com>
2023-11-06 08:56:44 -0300

    Agregado los test, create, list, delete products

--- src/test/java/com/proyecto/integrador/ProductTest.java
+++ src/test/java/com/proyecto/integrador/ProductTest.java
@@ -1,6 +1,7 @@
 package com.proyecto.integrador;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.proyecto.integrador.exceptions.BadRequestException;
 import com.proyecto.integrador.product.Product;
 import com.proyecto.integrador.product.ProductService;
 import com.proyecto.integrador.product.dto.ProductDTO;
@@ -14,10 +15,14 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc(addFilters = false) //addFilters = false --> No nos tenemos que autenticar
@@ -52,21 +57,41 @@ public class ProductTest {
         productoExistente.setName("Guitar");
         productService.createProduct(productoExistente);
 
-        // Intenta agregar un producto con el mismo nombre
+        // Intento agregar un producto con el mismo nombre
         Product nuevoProducto = new Product();
         nuevoProducto.setName("Guitar");
 
-        // Realiza la solicitud POST para agregar el nuevo producto
+        // Realizo la solicitud POST para agregar el nuevo producto
         MvcResult resultado = mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/products")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(asJsonString(nuevoProducto)))
-                .andExpect(MockMvcResultMatchers.status().isBadRequest()) // Espera un fallo
+                .andExpect(MockMvcResultMatchers.status().isBadRequest()) // Esperamos un fallo
                 .andReturn();
         String responseBody = resultado.getResponse().getContentAsString();
         assertTrue(responseBody.contains("The product already exists, use another name"));
     }
 
-    // Convierte un objeto a una cadena JSON
+    @Test
+    public void deleteProductTest() throws Exception {
+        // Agregamos un producto a la base de datos
+        ProductDTO productoAgregado = productService.createProduct(
+                new Product(1L, "Guitar", "Musical Instruments", "Fender", "Stratocaster", "A classic electric guitar.", 799.99f, Arrays.asList("[ ]"), 10, null));
+
+        // Obtenemos el ID del producto que se acaba de agregar
+        Long productId = productoAgregado.getId();
+
+        // Llamamos al método para eliminar el producto
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/products/" + productId)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isNoContent());
+
+        // Intento encontrar el producto por ID (debe arrojar una excepción)
+        assertThrows(BadRequestException.class, () -> productService.findProductById(productId));
+    }
+
+
+    // Método para convertir un objeto a una cadena JSON
     private static String asJsonString(final Object obj) {
         try {
             final ObjectMapper mapper = new ObjectMapper();
