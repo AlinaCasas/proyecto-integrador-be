1ef9548d746b5089da3826087f332b41921e833b
SimonPintos7 <89111447+SimonPintos7@users.noreply.github.com>
2023-11-19 17:16:50 -0300

    Update SecurityConfiguration.java

--- src/main/java/com/proyecto/integrador/config/SecurityConfiguration.java
+++ src/main/java/com/proyecto/integrador/config/SecurityConfiguration.java
@@ -70,7 +70,7 @@ public class SecurityConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("/**"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
         configuration.setExposedHeaders(Arrays.asList("Authorization"));
